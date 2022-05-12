6180cad737a9b42f5af24a1bd627e3e97cecbe7f
naila-mkenzi <nailamkenzi@gmail.com>
2022-05-13 00:36:02 +0200

    nnn

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React, { useState } from "react";
+
+
+import React, { useState } from 'react';
 import Connexion from "./connexion";
 
+
+
+
+
 function App() {
-  const adminUser = {
-    email: "naila@admin.com",
-    password: "admin123",
-  };
 
-  const [user, setUser] = useState({ name: ", email:" });
-  const [error, setError] = useState("");
+const adminUser = {
 
-  //methode de login
-  const Login = (details) => {
-    console.log(details);
-
-    // condition
-    if (
-      details.email == adminUser.email &&
-      details.password == adminUser.password
-    ) {
-      console.log("Login");
-      setUser({
-        name: details.name,
-        email: details.email,
-      });
-    } else {
-      console.log("Les informations sont incorrectes");
-      setError(" Remplir les champs pour connecter ! ");
-    }
-  };
-  //hello from abdelwahab
-
-  return (
-    <div className="App">
-      <Connexion Login={Login} error={error} />
-    </div>
-  );
+email: "naila@admin.com",
+password: "admin123"
 }
 
-export default App;
+const [user, setUser] = useState({name:", email:"});
+const [error, setError] =useState ("");
+ 
+//methode de login
+const Login = details => {
+  console.log(details);
+
+// condition
+if (details.email == adminUser.email && details.password == adminUser.password){
+console.log("Login"); 
+setUser({
+name: details.name,
+email: details.email
+});
+} 
+else {
+console.log("Les informations sont incorrectes");
+setError(" Remplir les champs pour connecter ! ");
+                          
+}
+}
+
+return (
+    <div className="App">
+     
+    
+       <Connexion Login={Login} error={error}/> 
+    </div>
+  );
+} export default App;
